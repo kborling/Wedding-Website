3b987c53a3dcb82f98ab84a196b1c5c7faf88b8a
kborling <kborling@gmail.com>
2014-10-25 22:11:43 -0700

    Changed Full Page Scroll features
    Added temporary navigation with some placeholders.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,39 @@
 $(document).ready(function(){
-   $(".main").onepage_scroll({
-      sectionContainer: "section",
-      easing: "ease",
-      animationTime: 1000,
-      pagination: true,
-      updateURL: true,
-      loop: true,
-      keyboard: true,   
-      responsiveFallback: false,                            
-      direction: "vertical"
-   });
+    $('#fullpage').fullpage({
+
+        //Navigation
+        menu: '#menu',
+        anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'fifthPage', 'sixthPage'],
+        navigation: true,
+        navigationPosition: 'top',
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+		//Scrolling
+        css3: true,
+        easing: 'easeInQuart',
+        easingcss3: 'ease',
+        loopHorizontal: true,
+        scrollingSpeed: 800,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+
+         //Design
+        verticalCentered: true,
+        resize : true,
+        sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff', '#1bbc9b'],
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        fixedElements: '#header, .footer',
+        responsive: 0,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide'
+
+    });
 });
